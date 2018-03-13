4b17fa83922fddc74c7ce0892e344d2ab412ca0c
Gian Carlo Pace <giancarlo.pace@gmail.com>
2018-03-13 16:36:58 +0100

    Creating an app context where all the object are wired. It's in the form of a factory method so that it can be decided what kind of AIS source to be used (ATM file or socket)

---
+++ ais-adaptor-app/src/main/java/eu/cise/adaptor/AppContext.java
@@ -0,0 +1,59 @@
+package eu.cise.adaptor;
+
+import eu.cise.adaptor.tbsalling.AISMessageConsumer;
+import eu.cise.adaptor.tbsalling.DefaultAISNormalizer;
+import eu.cise.adaptor.tbsalling.FileAISSource;
+import eu.cise.adaptor.tbsalling.SocketAISSource;
+import jrc.cise.gw.sending.Dispatcher;
+import org.aeonbits.owner.ConfigFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public interface AppContext {
+
+    AISSource buildSource();
+
+    class FileAppContext implements AppContext {
+
+        private final ClassPathXmlApplicationContext context;
+        private final AISAdaptorConfig config;
+        private final Dispatcher dispatcher;
+
+        public FileAppContext() {
+            context = new ClassPathXmlApplicationContext("/context-ais.xml");
+            config = ConfigFactory.create(AISAdaptorConfig.class);
+            dispatcher = context.getBean("signingDispatcher", Dispatcher.class);
+        }
+
+        @Override
+        public AISSource buildSource() {
+            return new FileAISSource(
+                    "/raw-ais/nmea-sample",
+                    new AISMessageConsumer(
+                            new DefaultAISNormalizer(),
+                            new DefaultAISProcessor(new DefaultTranslator(config), dispatcher, config)));
+        }
+    }
+
+    class SocketAppContext implements AppContext {
+
+        private final ClassPathXmlApplicationContext context;
+        private final AISAdaptorConfig config;
+        private final Dispatcher dispatcher;
+
+        public SocketAppContext() {
+            context = new ClassPathXmlApplicationContext("/context-ais.xml");
+            config = ConfigFactory.create(AISAdaptorConfig.class);
+            dispatcher = context.getBean("signingDispatcher", Dispatcher.class);
+        }
+
+        @Override
+        public AISSource buildSource() {
+            return new SocketAISSource(
+                    "localhost",
+                    8080,
+                    new AISMessageConsumer(
+                            new DefaultAISNormalizer(),
+                            new DefaultAISProcessor(new DefaultTranslator(config), dispatcher, config)));
+        }
+    }
+}
--- ais-adaptor-app/src/main/java/eu/cise/adaptor/MainApp.java
+++ ais-adaptor-app/src/main/java/eu/cise/adaptor/MainApp.java
@@ -1,39 +1,19 @@
 package eu.cise.adaptor;
 
-import eu.cise.adaptor.tbsalling.AISMessageConsumer;
-import eu.cise.adaptor.tbsalling.DefaultAISNormalizer;
-import eu.cise.adaptor.tbsalling.FileAISSource;
-import jrc.cise.gw.sending.Dispatcher;
-import org.aeonbits.owner.ConfigFactory;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 /**
  * Application entry point
  */
 public class MainApp {
 
     public static final String VERSION = "1.0";
-    private final AISSource aisConsumer;
+    private final AISSource aisSource;
     private final Banner banner;
-    private final ApplicationContext context;
-    private final Dispatcher dispatcher;
+    private final AppContext.FileAppContext appContext;
 
     public MainApp() {
-        this.banner = new Banner();
-        this.context = new ClassPathXmlApplicationContext("/context-ais.xml");
-
-//        this.aisConsumer = context.getBean("socketAISSource", AISSource.class);
-
-        AISAdaptorConfig config = ConfigFactory.create(AISAdaptorConfig.class);
-
-        this.dispatcher = context.getBean("signingDispatcher", Dispatcher.class);
-
-        this.aisConsumer = new FileAISSource(
-                "/raw-ais/nmea-sample",
-                new AISMessageConsumer(
-                        new DefaultAISNormalizer(),
-                        new DefaultAISProcessor(new DefaultTranslator(config), dispatcher, config)));
+        banner = new Banner();
+        appContext = new AppContext.FileAppContext();
+        aisSource = appContext.buildSource();
     }
 
     public static void main(String[] args) {
@@ -43,11 +23,7 @@ public class MainApp {
     public void run() {
         try {
             banner.print(VERSION);
-
-            System.out.println("Press a key to start consuming AIS Messages.");
-            System.in.read();
-
-            aisConsumer.run();
+            aisSource.run();
         } catch (Throwable e) {
             throw new AISAdaptorException(e);
         }
