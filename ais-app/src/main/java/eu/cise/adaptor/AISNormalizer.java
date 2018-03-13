e1385b628f753fc70d436498199dbaa409ce4b1b
Gian Carlo Pace <giancarlo.pace@gmail.com>
2018-03-13 18:35:35 +0100

    Generalizing the normalizer

--- ais-app/src/main/java/eu/cise/adaptor/AISNormalizer.java
+++ ais-app/src/main/java/eu/cise/adaptor/AISNormalizer.java
@@ -1,15 +1,13 @@
 package eu.cise.adaptor;
 
-import dk.tbsalling.aismessages.ais.messages.AISMessage;
-
 /**
  * What a normalizer does is to abstract from the AIS library used to read
  * the AIS streams.
- *
- * The normalizer will take as an input an {@link AISMessage}, that is the
- * representation of an AIS message coming from the tbsalling's library and
- * normalize it to a {@link AISMsg} that is a domain object.
+ * <p>
+ * The normalizer will take as an input a structure defined in a library that
+ * represent an AIS message coming will normalize it to a {@link AISMsg}
+ * that is a domain object.
  */
-public interface AISNormalizer {
-    AISMsg normalize(AISMessage m);
+public interface AISNormalizer<T> {
+    AISMsg normalize(T m);
 }
