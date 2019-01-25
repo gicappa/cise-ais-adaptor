57bccb7ff4cb9b3d226a83f850902841f53ce041
Gian Carlo Pace <giancarlo.pace@gmail.com>
2019-01-25 14:36:22 +0100

    Refactored

---
+++ ais-source-adapter/src/main/java/eu/cise/adaptor/sources/AuthTcpStreamGenerator.java
@@ -0,0 +1,114 @@
+package eu.cise.adaptor.sources;
+
+import eu.cise.adaptor.AisStreamGenerator;
+import eu.cise.adaptor.exceptions.AdaptorException;
+import org.aeonbits.owner.ConfigFactory;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.stream.Stream;
+
+import static java.lang.String.format;
+
+/**
+ * This class is used in a simple case of an authentication protocol where the generator needs
+ * to connect to a socket under a simple authentication protocol.
+ * The protocol is very simple and foresee that after the accept from the TCP server the client
+ * adaptor will send a single line on the TCP socket with the credentials (following a template
+ * configurable by a property) and wil receive an answer with a single line expressing success
+ * or failure of the authentication.
+ *
+ * In case of success the AIS message stream will start flowing, while in case of failure an
+ * exception will be thrown.
+ */
+@SuppressWarnings("unused")
+public class AuthTcpStreamGenerator implements AisStreamGenerator {
+
+    private static final AuthTcpAdaptorConfig config
+            = ConfigFactory.create(AuthTcpAdaptorConfig.class);
+    private final AisTcpStreamGenerator decorated;
+    private final BufferedReader reader;
+    private final PrintWriter writer;
+
+    public AuthTcpStreamGenerator() {
+        this(config.getAISSourceSocketHost(), config.getAISSourceSocketPort(), new Socket());
+    }
+
+    /**
+     * Construct the object, building an internal { AisTcpStreamGenerator }
+     * NOTE: this class could be more abstract and decorating a generic AisStreamGenerator if the
+     * latter could expose more abstraction and specialization. Before starting generalizing, I'd
+     * like to keep it simpler and add the generalization needed evaluating case by case.
+     *
+     * @param host the host listening for connections
+     * @param port the port opened to receive connections
+     * @param socket the socket object to use to open the connection
+     */
+    public AuthTcpStreamGenerator(String host, Integer port, Socket socket) {
+        try {
+            this.decorated = new AisTcpStreamGenerator(host, port, socket);
+            this.reader = getReader(socket);
+            this.writer = getWriter(socket);
+        } catch (IOException e) {
+            throw new AdaptorException(e);
+        }
+    }
+
+    /**
+     * Decorates the current {AisTcpStreamGenerator} sending to the socket a login line with a
+     * carriage return at the end with the credential formatted accordingly to the
+     * ais-source.login.request.template in the property file ais-adaptor.properties
+     * The first '%s' will be replaced by the username and the second %s will be replaced by the
+     * password.
+     *
+     * @return a stream of String of AIS messages, if the authentication is granted.
+     */
+    @Override
+    public Stream<String> generate() {
+        try {
+            writer.println(loginCommand(config.getTcpLoginUsername(), config.getTcpLoginPassword()));
+
+            String input = reader.readLine();
+
+            if (input == null || !input.equalsIgnoreCase(config.getTcpLoginSuccessTemplate()))
+                throw new AdaptorException("AIS Source: Authentication error: " + input);
+
+            return decorated.generate();
+        } catch (IOException e) {
+            throw new AdaptorException(e);
+        }
+    }
+
+    /**
+     * Format properly a String with a possible login command defined by a username and a password.
+     *
+     * @param username the login username to be passed to the TCP socket
+     * @param password the login password to be passed to the TCP socket
+     * @return the formatted command to login in the socket
+     */
+    private String loginCommand(String username, String password) {
+        return format(config.getTcpLoginRequestTemplate(), username, password);
+    }
+
+    /**
+     * Extract a BufferedReader out of a socket InputStream.
+     *
+     * @param socket the socket object
+     * @return the BufferedReader bound to the socket
+     * @throws IOException in case of a socket error
+     */
+    private BufferedReader getReader(Socket socket) throws IOException {
+        return new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    }
+
+    /**
+     * Extract a PrintWriter out of a socket OutputStream.
+     *
+     * @param socket the socket object
+     * @return the PrintWriter bound to the socket
+     * @throws IOException in case of a socket error
+     */
+    private PrintWriter getWriter(Socket socket) throws IOException {
+        return new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+    }
+}
