ff4f9a7c77649dcd84c2404fc6f7355347351e2d
Gian Carlo Pace <giancarlo.pace@ec.europa.eu>
2019-07-31 18:20:01 +0200

    Fixing a warning

--- cise-signature/src/test/java/eu/cise/signature/ResourceFileTest.java
+++ cise-signature/src/test/java/eu/cise/signature/ResourceFileTest.java
@@ -32,7 +32,6 @@ import eu.cise.signature.exceptions.SignatureEx;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import sun.misc.IOUtils;
 
 import java.io.File;
 import java.io.IOException;
