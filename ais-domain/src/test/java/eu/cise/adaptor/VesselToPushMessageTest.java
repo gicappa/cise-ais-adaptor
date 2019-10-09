74e320de07572c89647c4965b83daa34fcbb8d71
Gian Carlo Pace <giancarlo.pace@ec.europa.eu>
2019-10-09 16:14:36 +0200

    typo

--- ais-domain/src/test/java/eu/cise/adaptor/VesselToPushMessageTest.java
+++ ais-domain/src/test/java/eu/cise/adaptor/VesselToPushMessageTest.java
@@ -91,7 +91,7 @@ public class VesselToPushMessageTest {
   }
 
   @Test
-  public void it_doesent_specifies_a_recipient_in_a_push_subscribe() {
+  public void it_doesnt_specifies_a_recipient_in_a_push_subscribe() {
     vesselToPush = new VesselToPushMessage(configUsingSubscribe(), null);
 
     Push actual = vesselToPush.translate(singletonList(new Vessel()));
@@ -100,7 +100,7 @@ public class VesselToPushMessageTest {
   }
 
   @Test
-  public void it_doesent_specifies_a_profile_in_a_push_subscribe() {
+  public void it_doesnt_specifies_a_profile_in_a_push_subscribe() {
     vesselToPush = new VesselToPushMessage(configUsingSubscribe(), null);
 
     Push actual = vesselToPush.translate(singletonList(new Vessel()));
